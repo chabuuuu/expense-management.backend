f5e0d3f476a3b905aa745a00c033dadb2398d875
chabuuuu <haphuthinh332004@gmail.com>
2024-06-10 09:16:47 +0700

    fix(remove security token): finish

--- src/utils/firebase/firebase.instance.ts
+++ src/utils/firebase/firebase.instance.ts
@@ -1,9 +1,19 @@
 import fs from "fs";
 import firebase from "firebase-admin";
 
-const serviceAccount = JSON.parse(
-    fs.readFileSync("./google-key.json", "utf8")
-  );
+const serviceAccount : any = {
+  "type": process.env.type,
+  "project_id": process.env.project_id,
+  "private_key_id": process.env.private_key_id,
+  "private_key": process.env.private_key,
+  "client_email": process.env.client_email,
+  "client_id": process.env.client_id,
+  "auth_uri": process.env.auth_uri,
+  "token_uri": process.env.token_uri,
+  "auth_provider_x509_cert_url": process.env.auth_provider_x509_cert_url,
+  "client_x509_cert_url": process.env.client_x509_cert_url,
+  "universe_domain": process.env.universe_domain
+}
 
 const firebaseInstance = firebase.initializeApp({
     credential: firebase.credential.cert(serviceAccount),
